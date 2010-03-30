5f5688276c9b4bd72462f43b499c0002c6e7be5c
Bruno Diniz <bruno.diniz@oper20.fabrica.locaweb.com.br>
2010-03-30 17:32:07 -0300

    Ajustes no anuncio

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,6 +1,7 @@
 class Listing < ActiveRecord::Base
   belongs_to :job
   before_create :active_listing 
+	validates_numericality_of :day_count, :less_than_or_equal_to => 30, :greater_than_or_equal_to => 10
   
   def active
     d = self.actived_at.to_date - Time.now.to_date
