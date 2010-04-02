2e73cf12a7f7b898c3d4349e43247e8f2083ce1f
bruno <bruno@dev-infra6.(none)>
2010-04-02 11:30:45 -0300

    ajustes na tela de listagem de vagas publicas

--- spec/controllers/job_listings_controller_spec.rb
+++ spec/controllers/job_listings_controller_spec.rb
@@ -1,27 +1,28 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe JobListingsController do
-  
+
   it "should updated job listing" do
     l1 = Factory(:listing)
     l2 = Factory(:listing)
     l3 = Factory(:listing)
     l2.disable_listing
-    
+
     put :updated_listing
-    response.should render_template('index')
-    
+    response.should redirect_to(jobs_path)
+
     Listing.job_listings.size.should == 2
   end
-  
+
   it "should list job listing" do
     l1 = Factory(:listing)
     l2 = Factory(:listing)
     l3 = Factory(:listing)
     l2.disable_listing
-    
+
     get :index
     assigns[:job_listings].should == Listing.job_listings
   end
-  
+
 end
+
