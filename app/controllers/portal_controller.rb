2ae1f9430106d4505536e64ffc71a2987f1ce7a9
Bruno Diniz <diniz.bd@gmail.com>
2010-04-20 10:43:06 -0300

    Formatando style no form do candidato

--- app/controllers/portal_controller.rb
+++ app/controllers/portal_controller.rb
@@ -3,11 +3,6 @@ class PortalController < ActionController::Base
     @listings = Listing.published
   end
   
-  def show
-    @category = Category.find(params[:category_id])
-    @listing = Listing.published.find(params[:listing_id])
-  end
-  
   def category_listing
     @category = Category.find(params[:category_id])
     @listings = Listing.list_jobs_for_category(@category)
--- spec/controllers/portal_controller_spec.rb
+++ spec/controllers/portal_controller_spec.rb
@@ -11,14 +11,6 @@ describe PortalController do
     assigns[:listings].should == Listing.published
   end
   
-  it "should details listing published" do
-    l = Factory(:listing)
-    
-    get :show, :listing_id => l.id, :category_id => l.job.category.id
-    assigns[:listing].should == l
-    assigns[:category].should == l.job.category
-  end
-  
   it "should list listing in category" do
     l = Factory(:listing)
     
